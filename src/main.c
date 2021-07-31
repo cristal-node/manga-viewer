5ab83b8965a6125f275d021e3778aa522d479199
cristal-node <12prottoy2002@gmail.com>
2021-07-31 18:12:19 +0900

    dynamic memory introduced

--- src/main.c
+++ src/main.c
@@ -29,12 +29,12 @@
 
 
 
-#define BUFFER_SIZE 4096
+// #define BUFFER_SIZE 4096
 #define PATH_SIZE 256
 
-
-char	doc_html[BUFFER_SIZE],
-			doc_img[BUFFER_SIZE],
+long	doc_img_size;
+char	*doc_html,
+			*doc_img,
 			archive[PATH_SIZE],
 			destination[PATH_SIZE],
 			title[PATH_SIZE];
@@ -62,7 +62,7 @@ void create_dir(char *path){
   if (!mkdir(path, 0755))return;
   if(errno == EEXIST)
 		printf("\"%s\" exists! Aborting...\n", path);
-  exit(1);
+  exit(EXIT_FAILURE);
 }
 
 // https://stackoverflow.com/questions/10195343/
@@ -71,17 +71,17 @@ void cp(char *from, char *to) {
 	char tmp_to[PATH_SIZE];
 	strcpy(tmp_to, destination);
 	strcat(tmp_to, to);
-    // BUFSIZE default is 8192 bytes
-    // BUFSIZE of 1 means one chareter at time
-    // good values should fit to blocksize, like 1024 or 4096
-    // higher values reduce number of system calls
-    // size_t BUFFER_SIZE = 4096;
+  // BUFSIZE default is 8192 bytes
+  // BUFSIZE of 1 means one chareter at time
+  // good values should fit to blocksize, like 1024 or 4096
+  // higher values reduce number of system calls
+  // size_t BUFFER_SIZE = 4096;
 
-    char buf[BUFSIZ];
-    size_t size;
+  char buf[BUFSIZ];
+  size_t size;
 
-    FILE* source = fopen(from, "rb");
-    FILE* dest = fopen(tmp_to, "wb");
+  FILE* source = fopen(from, "rb");
+  FILE* dest = fopen(tmp_to, "wb");
 
 	if(!source){
 		printf("can't open: %s", from);
@@ -92,23 +92,23 @@ void cp(char *from, char *to) {
 		exit(EXIT_FAILURE);
 	}
 
-    // clean and more secure
-    // feof(FILE* stream) returns non-zero if the end of file indicator for stream is set
+  // clean and more secure
+  // feof(FILE* stream) returns non-zero if the end of file indicator for stream is set
 
-    while ((size = fread(buf, 1, BUFSIZ, source))) {
-        fwrite(buf, 1, size, dest);
-    }
+  while ((size = fread(buf, 1, BUFSIZ, source))) {
+  	fwrite(buf, 1, size, dest);
+  }
 
-    fclose(source);
-    fclose(dest);
+  fclose(source);
+  fclose(dest);
 }
 
 
 
 void img(const char *image){
-	char tmp[PATH_SIZE];
-	snprintf(tmp, PATH_SIZE, "<img src=\"images/%s\"/>\n", image);
-	strcat(doc_img, tmp);
+	char tmp_buff[30];
+	snprintf(tmp_buff, 30, "<img src=\"images/%s\"/>\n", image);
+	strcat(doc_img, tmp_buff);
 }
 
 void html_ready(){
@@ -129,17 +129,17 @@ void html_ready(){
 	// the beginning of the file 
 	fseek(fp, 0L, SEEK_SET);	
 	 
-	// grab sufficient memory for the 
-	// buffer to hold the text 
-	tmp_buff = (char*)calloc(numbytes, sizeof(char));	
+	// grab sufficient memory for the
+	// buffer to hold the text
+	tmp_buff = (char*)calloc(numbytes, sizeof(char));
 	 
-	// memory error 
+	// memory error
 	if(tmp_buff == NULL){
 		printf("not sufficient memory");
 		exit(EXIT_FAILURE);
 	}
 	 
-	// copy all the text into the buffer 
+	// copy all the text into the buffer
 	fread(tmp_buff, sizeof(char), numbytes, fp);
 	fclose(fp);
 	 
@@ -154,10 +154,17 @@ void html_ready(){
 	// fread(tmp_buff, BUFFER_SIZE, 1, fp);
 	// fclose(fp);
 
-	snprintf(doc_html, BUFFER_SIZE, tmp_buff, title, doc_img);
+	numbytes = numbytes + doc_img_size;
+	doc_html = (char*)calloc(numbytes, sizeof(char));
+	if(doc_html == NULL){
+		printf("not sufficient memory");
+		exit(EXIT_FAILURE);
+	}
+	snprintf(doc_html, numbytes, tmp_buff, title, doc_img);
 
 	// free the memory we used for the buffer 
 	free(tmp_buff);
+	free(doc_img);
 
 
 	// html write
@@ -167,11 +174,12 @@ void html_ready(){
 	fp = fopen(tmp_path, "w");
 	fwrite(doc_html, sizeof(char), strlen(doc_html), fp);
 	fclose(fp);
+	free(doc_html);
 }
 
 
 void unzip(){
-	char buf[BUFFER_SIZE];	// buffer
+	char buf[BUFSIZ];	// buffer
 	struct zip *za;					// zip archive
 	struct zip_file *zf;		// zip file
 	struct zip_stat sb;			// zip file stat
@@ -188,9 +196,24 @@ void unzip(){
     exit(EXIT_FAILURE);
   }
 
+  int index_size = zip_get_num_entries(za, 0);
+  if(!index_size){
+  	fprintf(stderr, "the archive is empty");
+  	if (zip_close(za) == -1)
+  		fprintf(stderr, "Can't close archive: %s\n", archive);
+  	exit(EXIT_FAILURE);
+  }
+  doc_img_size = 30L * index_size;
+	doc_img = (char*)calloc(doc_img_size, sizeof(char));
+
+	if(doc_img == NULL){
+		fprintf(stderr, "not sufficient memory");
+		exit(EXIT_FAILURE);
+	}
+
   create_dir(images_path);
 
-	for (int i = 0; i < zip_get_num_entries(za, 0); i++) {
+	for (int i = 0; i < index_size; i++) {
   	if (zip_stat_index(za, i, 0, &sb)) {
   		printf("File[%s] Line[%d]\n", __FILE__, __LINE__);
   		continue;
@@ -199,7 +222,7 @@ void unzip(){
     if (sb.name[strlen(sb.name)-1] == 0x2f){
     	strcpy(tmp_path, images_path);
       strcat(tmp_path, sb.name);
-      printf("path: %s", tmp_path);
+      // printf("path: %s", tmp_path);
       create_dir(tmp_path);
       continue;
     }
@@ -222,7 +245,7 @@ void unzip(){
 
 		int sum = 0;
 		while (sum != sb.size) {
-    	int len = zip_fread(zf, buf, BUFFER_SIZE);
+    	int len = zip_fread(zf, buf, BUFSIZ);
       if (len < 0){
       	fprintf(stderr, "error writing buffer: %s\n", sb.name);
         continue;
